0e9293b81ad65b3095b4000742719576fa0514f5
Gabriel Saiago <grsaiago@gmail.com>
2025-02-25 17:44:46 -0300

    got it! Didn't need the previous link to do so. Just added a %c

--- c/ex01/main.c
+++ c/ex01/main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#define STR "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#define STR %c%s%c%cint main(void) {%c%cint fd = open(%cGrace_kid.c%c, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG);%c%cwrite(fd, STR, sizeof(STR));%c%cdprintf(fd, STR, 10, 10, 10, 34, STR, 34, 10, 10, 9, 34, 34, 10, 9, 10, 9);}"
+int main(void) {
+	int fd = open("Grace_kid.c", O_RDWR | O_CREAT, S_IRWXU | S_IRWXG);
+	dprintf(fd, STR, 10, 10, 10, 34, STR, 34, 10, 10, 9, 34, 34, 10, 9, 10, 9);}
 
-#define STR "#include <stdio.h>%c%c#define STR %s%cint main(void) { printf(STR, 10, 10, STR, 10); }"
-int main(void) { printf(STR, 10, 10, STR, 10); }
