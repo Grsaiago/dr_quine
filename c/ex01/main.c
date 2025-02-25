2a3d408e3892df04a1910de087bc7b1df4b8744c
Gabriel Saiago <grsaiago@gmail.com>
2025-02-25 19:09:40 -0300

    feat(ex01): Add final in C with comment yipiiiiiiii

--- c/ex01/main.c
+++ c/ex01/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define STR "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#define STR %c%s%c%c#define FT() int main(void) {int fd = open(%cGrace_kid.c%c, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG); dprintf(fd, STR, 10, 10, 10, 34, STR, 34, 10, 34, 34, 10);}%cFT()"
-#define FT() int main(void) {int fd = open("Grace_kid.c", O_RDWR | O_CREAT, S_IRWXU | S_IRWXG); dprintf(fd, STR, 10, 10, 10, 34, STR, 34, 10, 34, 34, 10);}
+/*
+ * um comentário top
+*/
+#define STR "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c*%c * um comentário top%c*/%c#define STR %c%s%c%c#define FT() int main(void) {int fd = open(%cGrace_kid.c%c, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG); dprintf(fd, STR, 10, 10, 10, 47, 10, 10, 10, 34, STR, 34, 10, 34, 34, 10);}%cFT()"
+#define FT() int main(void) {int fd = open("Grace_kid.c", O_RDWR | O_CREAT, S_IRWXU | S_IRWXG); dprintf(fd, STR, 10, 10, 10, 47, 10, 10, 10, 34, STR, 34, 10, 34, 34, 10);}
 FT()
