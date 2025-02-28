1d027bd8cb3614d1af289d0c7a0a9b33ecb40e7c
Gabriel Saiago <grsaiago@gmail.com>
2025-02-28 16:37:36 -0300

    fix(ex00-c): Add a function to be called in main

--- c/ex00/Colleen.c
+++ c/ex00/Colleen.c
@@ -2,8 +2,10 @@
 /*
  * um comentário
 */
+void func(void) {return;}
 int main() {
 	/*e outro*/
-	char	*f="#include <stdio.h>%c/*%c * um comentário%c*/%cint main() {%c%c/*e outro*/%c%cchar%c*f=%c%s%c;%c%cprintf(f, 10, 10, 10, 10, 10, 9, 10, 9, 9, 34, f, 34, 10, 9, 10, 10);%c}%c";
-	printf(f, 10, 10, 10, 10, 10, 9, 10, 9, 9, 34, f, 34, 10, 9, 10, 10);
+	char	*f="#include <stdio.h>%c/*%c * um comentário%c*/%cvoid func(void) {return;}%cint main() {%c%c/*e outro*/%c%cchar%c*f=%c%s%c;%c%cfunc();%c%cprintf(f, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 34, f, 34, 10, 9, 10, 9, 10, 10);%c}%c";
+	func();
+	printf(f, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 34, f, 34, 10, 9, 10, 9, 10, 10);
 }
