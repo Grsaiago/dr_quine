66f0b88b6324d3f885d44afd4ec148509b2ffaeb
Gabriel Saiago <grsaiago@gmail.com>
2025-03-12 21:12:22 -0300

    fix(project): Final edits

--- c/ex00/Colleen.c
+++ c/ex00/Colleen.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- * um comentário
+  um comentário
 */
 void func(void) {return;}
 int main() {
 	/*e outro*/
-	char	*f="#include <stdio.h>%c/*%c * um comentário%c*/%cvoid func(void) {return;}%cint main() {%c%c/*e outro*/%c%cchar%c*f=%c%s%c;%c%cfunc();%c%cprintf(f, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 34, f, 34, 10, 9, 10, 9, 10, 10);%c}%c";
+	char	*f="#include <stdio.h>%c/*%c  um comentário%c*/%cvoid func(void) {return;}%cint main() {%c%c/*e outro*/%c%cchar%c*f=%c%s%c;%c%cfunc();%c%cprintf(f, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 34, f, 34, 10, 9, 10, 9, 10, 10);%c}%c";
 	func();
 	printf(f, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 34, f, 34, 10, 9, 10, 9, 10, 10);
 }
--- c/ex01/Grace.c
+++ c/ex01/Grace.c
@@ -2,8 +2,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 /*
- * um comentário top
+  um comentário top
 */
-#define STR "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c*%c * um comentário top%c*/%c#define STR %c%s%c%c#define FT() int main(void) {int fd = open(%cGrace_kid.c%c, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG); dprintf(fd, STR, 10, 10, 10, 47, 10, 10, 10, 34, STR, 34, 10, 34, 34, 10);}%cFT()"
+#define STR "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c*%c  um comentário top%c*/%c#define STR %c%s%c%c#define FT() int main(void) {int fd = open(%cGrace_kid.c%c, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG); dprintf(fd, STR, 10, 10, 10, 47, 10, 10, 10, 34, STR, 34, 10, 34, 34, 10);}%cFT()"
 #define FT() int main(void) {int fd = open("Grace_kid.c", O_RDWR | O_CREAT, S_IRWXU | S_IRWXG); dprintf(fd, STR, 10, 10, 10, 47, 10, 10, 10, 34, STR, 34, 10, 34, 34, 10);}
 FT()
